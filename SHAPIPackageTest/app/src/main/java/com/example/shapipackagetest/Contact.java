0c41d2fac50497ac5396627503dce2591f7227bf
Mariel Trajano <marielqt98@gmail.com>
2019-10-02 07:00:26 -0700

    Add files via upload

---
+++ SHAPIPackageTest/app/src/main/java/com/example/shapipackagetest/Contact.java
@@ -0,0 +1,47 @@
+package com.example.shapipackagetest;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private boolean textAlert;
+    private boolean emailAlert;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public boolean isTextAlert() {
+        return textAlert;
+    }
+    public void setTextAlert(boolean textAlert) {
+        this.textAlert = textAlert;
+    }
+    public boolean isEmailAlert() {
+        return emailAlert;
+    }
+    public void setEmailAlert(boolean emailAlert) {
+        this.emailAlert = emailAlert;
+    }
+}
+
